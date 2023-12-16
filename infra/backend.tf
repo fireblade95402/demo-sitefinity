450babd84c2abca4978b1cb04fd14511d8b4dfb5
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-12-16 00:34:06 +0000

    g

--- infra/backend.tf
+++ infra/backend.tf
@@ -12,7 +12,6 @@ terraform {
     storage_account_name = "sitefinitysharedmwg"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
   }
 }
 
