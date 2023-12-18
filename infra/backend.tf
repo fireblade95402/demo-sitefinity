a471d4178e0414cec8c8c9f53af047d441f4d981
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-12-18 14:36:57 +0000

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -8,9 +8,9 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "demo-sitefinity"
-    storage_account_name = "sitefinitysharedmwg"
-    container_name       = "tfstate"
+    resource_group_name  = "Shared"
+    storage_account_name = "sharedstoremwg"
+    container_name       = "sitefinitytfstate"
     key                  = "terraform.tfstate"
   }
 }
